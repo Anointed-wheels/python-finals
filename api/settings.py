4df2333ec095a14efa8f89809cfbc873b2b6a1e2
Anointed-wheels <toluleyeoluwaseyi@gmail.com>
2025-05-26 15:41:36 +0100

    python-finals

--- api/settings.py
+++ api/settings.py
@@ -163,6 +163,7 @@ AUTH_USER_MODEL ="authentication.CustomUser"
 STATIC_URL = 'static/'
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
