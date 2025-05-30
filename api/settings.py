b791773e68fadb4c6eab29a3873338ade76b267e
Anointed-wheels <toluleyeoluwaseyi@gmail.com>
2025-05-30 14:22:53 +0100

    python-finals

--- api/settings.py
+++ api/settings.py
@@ -166,7 +166,7 @@ STORAGES = {
 EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
